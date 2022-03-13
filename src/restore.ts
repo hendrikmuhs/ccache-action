005b441e86804b4bc6df7a82f50851dbde0d19a0
Jonas Haag <jonas@lophus.org>
2022-03-13 12:20:33 +0200

    Add Sccache support + add restore-keys + don't save empty cache (#47)
    Add Sccache (windows) support

--- src/restore.ts
+++ src/restore.ts
@@ -4,69 +4,128 @@ import * as exec from "@actions/exec";
 import * as process from "process";
 import * as cache from "@actions/cache";
 
+const SELF_CI = process.env["CCACHE_ACTION_CI"] === "true"
+
 // based on https://cristianadam.eu/20200113/speeding-up-c-plus-plus-github-actions-using-ccache/
 
-async function install() {
-  if (process.platform === "darwin") {
-    await exec.exec("brew install ccache");
-  } else {
-    await exec.exec("sudo apt-get install -y ccache");
-  }
-}
+async function restore(ccacheVariant : string) : Promise<void> {
+  const inputs = {
+    primaryKey: core.getInput("key"),
+    // https://github.com/actions/cache/blob/73cb7e04054996a98d39095c0b7821a73fb5b3ea/src/utils/actionUtils.ts#L56
+    restoreKeys: core.getInput("restore-keys").split("\n").map(s => s.trim()).filter(x => x !== "")
+  };
 
-async function restore() {
-  let restoreKey = `ccache-`;
+  const keyPrefix = ccacheVariant + "-";
+  const primaryKey = inputs.primaryKey ? keyPrefix + inputs.primaryKey + "-" : keyPrefix;
+  const restoreKeys = inputs.restoreKeys.map(k => keyPrefix + k + "-")
+  const paths = [`.${ccacheVariant}`];
 
-  let inputKey = core.getInput("key");
-  if (inputKey) {
-    restoreKey += `${inputKey}-`;
-  }
+  core.saveState("primaryKey", primaryKey);
 
-  const restoreKeys = [
-    restoreKey
-  ]
-  
-  const key = restoreKey + new Date().toISOString();
-
-  const paths = [
-    '.ccache'
-  ]  
-
-  core.saveState("primaryKey", key)
-  const restoredWith = await cache.restoreCache(paths, key, restoreKeys)
+  const restoredWith = await cache.restoreCache(paths, primaryKey, restoreKeys);
   if (restoredWith) {
     core.info(`Restored from cache key "${restoredWith}".`);
+    if (SELF_CI) {
+      core.setOutput("test-cache-hit", true)
+    }
   } else {
     core.info("No cache found.");
+    if (SELF_CI) {
+      core.setOutput("test-cache-hit", false)
+    }
   }
 }
 
-async function configure() {
+async function configure(ccacheVariant : string) : Promise<void> {
   const ghWorkSpace = process.env.GITHUB_WORKSPACE;
   const maxSize = core.getInput('max-size');
   
-  core.info("Configure ccache");
-  await exec.exec("ccache --set-config=cache_dir=" + ghWorkSpace + "/.ccache");
-  await exec.exec("ccache --set-config=max_size=" + maxSize);
-  await exec.exec("ccache --set-config=compression=true");
+  core.info(`Configure ${ccacheVariant}`);
+  if (ccacheVariant === "ccache") {
+    await exec.exec(`ccache --set-config=cache_dir=${ghWorkSpace}/.ccache}`);
+    await exec.exec(`ccache --set-config=max_size=${maxSize}`);
+    await exec.exec(`ccache --set-config=compression=true`);
+    core.info("Cccache config:");
+    await exec.exec("ccache -p");
+  } else {
+    const options = `SCCACHE_IDLE_TIMEOUT=999999 SCCACHE_DIR="${ghWorkSpace}"/.sccache SCCACHE_CACHE_SIZE="${maxSize}"`;
+    await exec.exec(`env ${options} sccache --start-server`);
+  }
 
-  core.info("Ccache config:")
-  await exec.exec("ccache -p");
+}
+
+async function installCcacheMac() : Promise<void> {
+  await exec.exec("brew install ccache");
+}
+
+async function installCcacheLinux() : Promise<void> {
+  await exec.exec("sudo apt-get install -y ccache");
+}
+
+async function installCcacheWindows() : Promise<void> {
+  throw Error("Ccache is not available for Windows, use 'variant: sccache'")
+}
+
+async function installSccacheMac() : Promise<void> {
+  await exec.exec("brew install sccache");
+}
+
+async function installSccacheLinux() : Promise<void> {
+  await installSccacheFromGitHub(
+    "v0.2.15",
+    "x86_64-unknown-linux-musl",
+    "e5d03a9aa3b9fac7e490391bbe22d4f42c840d31ef9eaf127a03101930cbb7ca",
+    "/usr/local/bin/",
+    "sccache"
+  );
+}
+
+async function installSccacheWindows() : Promise<void> {
+  await installSccacheFromGitHub(
+    "v0.2.15",
+    "x86_64-pc-windows-msvc",
+    "e5d03a9aa3b9fac7e490391bbe22d4f42c840d31ef9eaf127a03101930cbb7ca",
+    // TODO find a better place
+    "C:\\Users\\runneradmin\\.cargo\\bin",
+    "sccache.exe"
+  );
+}
+
+async function installSccacheFromGitHub(version : string, artifactName : string, sha256 : string, binPath : string, binName : string) : Promise<void> {
+  const archiveName = `sccache-${version}-${artifactName}`;
+  const url = `https://github.com/mozilla/sccache/releases/download/${version}/${archiveName}.tar.gz`;
+  await exec.exec("sh", ["-c", `curl -L '${url}' | tar xzf - -O '${archiveName}/${binName}' > '${binPath}/${binName}'`]);
+  await exec.exec(`echo "${sha256}  ${binPath}/${binName}" | sha256sum -c`);
+  await exec.exec(`chmod +x ${binPath}/${binName}`);
 }
 
 async function run() : Promise<void> {
   try {
-    let ccachePath = await io.which("ccache");
+    const ccacheVariant = core.getInput("variant");
+    core.saveState("ccacheVariant", ccacheVariant);
+    let ccachePath = await io.which(ccacheVariant);
     if (!ccachePath) {
-      core.info(`Install ccache`);
-      await install();
-      ccachePath = await io.which("ccache", true);
+      core.info(`Install ${ccacheVariant}`);
+      const installer = {
+        ["ccache,linux"]: installCcacheLinux,
+        ["ccache,darwin"]: installCcacheMac,
+        ["ccache,win32"]: installCcacheWindows,
+        ["sccache,linux"]: installSccacheLinux,
+        ["sccache,darwin"]: installSccacheMac,
+        ["sccache,win32"]: installSccacheWindows,
+      }[[ccacheVariant, process.platform].join()];
+      if (!installer) {
+        throw Error(`Unsupported platform: ${process.platform}`)
+      }
+      await installer();
+      core.info(await io.which(ccacheVariant + ".exe"));
+      ccachePath = await io.which(ccacheVariant, true);
     }
 
-    await restore();
-    await configure();
+    await restore(ccacheVariant);
+    await configure(ccacheVariant);
 
-    await exec.exec("ccache -z");
+    await exec.exec(`${ccacheVariant} -z`);
   } catch (error) {
     core.setFailed(`Restoring cache failed: ${error}`);
   }
--- src/save.ts
+++ src/save.ts
@@ -2,35 +2,60 @@ import * as core from "@actions/core";
 import * as cache from "@actions/cache";
 import * as exec from "@actions/exec";
 
-async function run() : Promise<void> {
-  try{
-    core.info("Ccache stats:")
-    let verbosity = '';
-    const inputVerbose = core.getInput("verbose");
-    switch (inputVerbose) {
-        case '0':
-            break;
-
-        case '1':
-            verbosity = ' -v';
-            break;
-
-        case '2':
-            verbosity = ' -vv';
-            break;
-
-        default:
-            core.warning(`Invalid value "${inputVerbose}" of "verbose" option ignored.`);
+async function ccacheIsEmpty(ccacheVariant : string, ccacheKnowsVerbosityFlag : boolean) : Promise<boolean> {
+  if (ccacheVariant === "ccache") {
+    if (ccacheKnowsVerbosityFlag) {
+      return !!(await exec.getExecOutput("ccache -s -v")).stdout.match(/Files:.+0/);
+    } else {
+      return !!(await exec.getExecOutput("ccache -s")).stdout.match(/files in cache.+0/)
     }
-    await exec.exec(`ccache -s${verbosity}`);
+  } else {
+    return !!(await exec.getExecOutput("sccache -s")).stdout.match(/Cache size.+0 bytes/);
+  }
+}
 
-    const key = core.getState("primaryKey") + new Date().toISOString();
-    const paths = [
-      '.ccache'
-    ]
-  
-    core.info(`Save cache using key "${key}".`)
-    await cache.saveCache(paths, key);
+async function getVerbosity(verbositySetting : string) : Promise<string> {
+  switch (verbositySetting) {
+    case '0':
+      return '';
+
+    case '1':
+      return ' -v';
+
+    case '2':
+      return ' -vv';
+
+    default:
+      core.warning(`Invalid value "${verbositySetting}" of "verbose" option ignored.`);
+      return '';
+  }
+}
+
+async function run() : Promise<void> {
+  try {
+    const ccacheVariant = core.getState("ccacheVariant");
+    const primaryKey = core.getState("primaryKey");
+    if (!ccacheVariant || !primaryKey) {
+      core.notice("ccache setup failed, skipping saving.");
+      return;
+    }
+
+    // Some versions of ccache do not support --verbose
+    const ccacheKnowsVerbosityFlag = !!(await exec.getExecOutput(`${ccacheVariant} --help`)).stdout.includes("--verbose");
+
+    core.info(`${ccacheVariant} stats:`);
+    const verbosity = ccacheKnowsVerbosityFlag ? await getVerbosity(core.getInput("verbose")) : '';
+    await exec.exec(`${ccacheVariant} -s${verbosity}`);
+
+    if (await ccacheIsEmpty(ccacheVariant, ccacheKnowsVerbosityFlag)) {
+      core.info("Not saving cache because no objects are cached.");
+    } else {
+      const saveKey = primaryKey + new Date().toISOString();
+      const paths = [`.${ccacheVariant}`];
+    
+      core.info(`Save cache using key "${saveKey}".`);
+      await cache.saveCache(paths, saveKey);
+    }
   } catch (error) {
     core.setFailed(`Saving cache failed: ${error}`);
   }
