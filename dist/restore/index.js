3179ba60b86622a2d80d9018b8fc38cb6b278111
berlin4apk <83521068+berlin4apk@users.noreply.github.com>
2023-04-09 20:53:38 +0200

    update dist

--- dist/restore/index.js
+++ dist/restore/index.js
@@ -59413,7 +59413,7 @@ async function downloadAndExtract(url, srcFile, dstFile) {
     //    tmpdirname: `${os.tmpdir()}${pathsep}picturama${pathsep}`,
     //    const tmpdirname = os.tmpdir() + '/knex-test-';
     // working but no template    const tmpdirname = (path.join(os.tmpdir() + `${pathsepstr}knex-test-`));
-    const tmpdirname = (path__WEBPACK_IMPORTED_MODULE_3___default().join(os__WEBPACK_IMPORTED_MODULE_2___default().tmpdir(), `${pathsepstr}knex-test-`));
+    const tmpdirname = fs__WEBPACK_IMPORTED_MODULE_1___default().mkdtempSync(path__WEBPACK_IMPORTED_MODULE_3___default().join(os__WEBPACK_IMPORTED_MODULE_2___default().tmpdir(), `${pathsepstr}knex-test-`));
     //    const tmpdirname = (path.join(os.tmpdir(), `${pathsepstr}knex-test-`));
     fs__WEBPACK_IMPORTED_MODULE_1___default().mkdirSync(tmpdirname, { recursive: true });
     //    const tmpdirname3 = fs.mkdtempSync(path.join(os.tmpdir(), `XXXXXX${pathsep}`));
--- src/restore.ts
+++ src/restore.ts
@@ -364,7 +364,7 @@ async function downloadAndExtract (url : string, srcFile : string, dstFile : str
 //    tmpdirname: `${os.tmpdir()}${pathsep}picturama${pathsep}`,
 //    const tmpdirname = os.tmpdir() + '/knex-test-';
 // working but no template    const tmpdirname = (path.join(os.tmpdir() + `${pathsepstr}knex-test-`));
-   const tmpdirname = (path.join(os.tmpdir(), `${pathsepstr}knex-test-`));
+   const tmpdirname = fs.mkdtempSync(path.join(os.tmpdir(), `${pathsepstr}knex-test-`));
 //    const tmpdirname = (path.join(os.tmpdir(), `${pathsepstr}knex-test-`));
           fs.mkdirSync(tmpdirname, { recursive: true });
 //    const tmpdirname3 = fs.mkdtempSync(path.join(os.tmpdir(), `XXXXXX${pathsep}`));
