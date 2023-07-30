920c8a57119938f79ef68208623f9d7b8303ddd5
botanicvelious <cyberempires@gmail.com>
2023-07-30 03:07:33 -0600

    compile ts

--- dist/restore/index.js
+++ dist/restore/index.js
@@ -59505,6 +59505,7 @@ async function runInner() {
     core.saveState("ccacheVariant", ccacheVariant);
     core.saveState("shouldSave", core.getBooleanInput("save"));
     core.saveState("appendTimestamp", core.getBooleanInput("append-timestamp"));
+	core.saveState("shouldRestore", core.getBooleanInput("restore"));
     let ccachePath = await io.which(ccacheVariant);
     if (!ccachePath) {
         core.startGroup(`Install ${ccacheVariant}`);
@@ -59525,7 +59526,12 @@ async function runInner() {
         core.endGroup();
     }
     core.startGroup("Restore cache");
-    await restore(ccacheVariant);
+    if (core.getState("shouldRestore") == "false") {
+        core.info("Restore set to false, skip restoring cache.");
+    }
+    else {
+        await restore(ccacheVariant);
+    }
     core.endGroup();
     core.startGroup(`Configure ${ccacheVariant}`);
     await configure(ccacheVariant);
