474f1fc59d2efe7b3eb14a5c8f35c667136d0af7
botanicvelious <cyberempires@gmail.com>
2023-07-30 05:36:21 -0600

    Update index.js

--- dist/restore/index.js
+++ dist/restore/index.js
@@ -59393,7 +59393,6 @@ async function restore(ccacheVariant) {
     const restoreKeys = inputs.restoreKeys.map(k => keyPrefix + k + "-");
     const paths = [`.${ccacheVariant}`];
     core.saveState("primaryKey", primaryKey);
-	
     const shouldRestore = core.getBooleanInput("restore");
     if (!shouldRestore) {
         core.info("Restore set to false, skip restoring cache.");
