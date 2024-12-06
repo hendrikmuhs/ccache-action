6f387851dc686da4ddc9833a1d7085535bd4d02f
Andrew Marshall <planetmarshall@users.noreply.github.com>
2024-12-06 10:37:33 +0000

    Add option to add stats to Job Summary page (#263)
    Adds a job-summary option that adds the stats to the Job Summary section of a workflow run. Requires CCache 4.10+

---
+++ __tests__/common.test.ts
@@ -0,0 +1,77 @@
+import * as common  from '../src/common';
+
+
+describe('ccache common', () => {
+    test('parse version string from ccache output', () => {
+        const ccacheOutput = `ccache version 4.10.2
+Features: avx2 file-storage http-storage redis+unix-storage redis-storage
+
+Copyright (C) 2002-2007 Andrew Tridgell
+Copyright (C) 2009-2024 Joel Rosdahl and other contributors
+
+See <https://ccache.dev/credits.html> for a complete list of contributors.
+
+This program is free software; etc etc.`;
+        expect(common.parseCCacheVersion(ccacheOutput)).toStrictEqual([4, 10, 2]);
+    });
+
+    test('format JSON stats output as table data', () => {
+        const stats = `{
+          "autoconf_test": 0,
+          "bad_compiler_arguments": 0,
+          "bad_input_file": 0,
+          "bad_output_file": 0,
+          "cache_miss": 3965,
+          "cache_size_kibibyte": 3511716,
+          "called_for_link": 0,
+          "called_for_preprocessing": 0,
+          "cleanups_performed": 0,
+          "compile_failed": 0,
+          "compiler_check_failed": 0,
+          "compiler_produced_empty_output": 0,
+          "compiler_produced_no_output": 0,
+          "compiler_produced_stdout": 0,
+          "could_not_find_compiler": 0,
+          "could_not_use_modules": 0,
+          "could_not_use_precompiled_header": 0,
+          "direct_cache_hit": 254,
+          "direct_cache_miss": 3965,
+          "disabled": 0,
+          "error_hashing_extra_file": 0,
+          "files_in_cache": 16868,
+          "internal_error": 0,
+          "local_storage_hit": 254,
+          "local_storage_miss": 3965,
+          "local_storage_read_hit": 508,
+          "local_storage_read_miss": 7930,
+          "local_storage_write": 7930,
+          "max_cache_size_kibibyte": 5242880,
+          "max_files_in_cache": 0,
+          "missing_cache_file": 0,
+          "modified_input_file": 0,
+          "multiple_source_files": 0,
+          "no_input_file": 0,
+          "output_to_stdout": 0,
+          "preprocessed_cache_hit": 0,
+          "preprocessed_cache_miss": 3965,
+          "preprocessor_error": 0,
+          "recache": 0,
+          "remote_storage_error": 0,
+          "remote_storage_hit": 0,
+          "remote_storage_miss": 0,
+          "remote_storage_read_hit": 0,
+          "remote_storage_read_miss": 0,
+          "remote_storage_timeout": 0,
+          "remote_storage_write": 0,
+          "stats_updated_timestamp": 1732222833,
+          "stats_zeroed_timestamp": 1732201768,
+          "unsupported_code_directive": 0,
+          "unsupported_compiler_option": 0,
+          "unsupported_environment_variable": 0,
+          "unsupported_source_language": 0
+        }`
+        expect(common.formatStatsAsTable(stats)).toStrictEqual([[
+            {data: "Cache hits", header: true }, "254 / 4219", "6.02%"
+        ]]);
+    })
+});
--- src/common.ts
+++ src/common.ts
@@ -1,4 +1,42 @@
 import path from "path";
+import {SummaryTableRow} from "@actions/core/lib/summary";
+
+type Version = [number,number,number];
+
+/**
+ * Parse the output of ccache --version to extract the semantic version components
+ * @param ccacheOutput
+ */
+export function parseCCacheVersion(ccacheOutput: string) : Version | null {
+    const firstLine = ccacheOutput.split("\n", 1)[0];
+    // short version of https://semver.org/#is-there-a-suggested-regular-expression-regex-to-check-a-semver-string
+    const semver = /(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)/;
+    const result = firstLine.match(semver);
+
+    if (!result) {
+        return null;
+    }
+
+    if (result.length != 4) {
+        return null;
+    }
+
+    return [Number.parseInt(result[1]), Number.parseInt(result[2]), Number.parseInt(result[3])];
+}
+
+export function formatStatsAsTable(statsJson: string) : SummaryTableRow[] | null {
+    const stats = JSON.parse(statsJson);
+    if (stats === undefined) {
+        return null;
+    }
+    // @ts-ignore
+    const hits = stats["direct_cache_hit"] + stats["preprocessed_cache_hit"];
+    const misses = stats["cache_miss"];
+    const total = hits + misses;
+    return [
+        [{data: "Cache hits", header: true}, `${hits} / ${total}`, `${((hits / total) * 100).toPrecision(3)}%`]
+    ];
+}
 
 export function cacheDir(ccacheVariant: string): string {
     const ghWorkSpace = process.env.GITHUB_WORKSPACE || "unreachable, make ncc happy";
